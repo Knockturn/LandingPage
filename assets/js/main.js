7fdd32cac7ae4f7cc069bd5c7fc5779ebfbe6dc3
Phi <pkbrauner@gmail.com>
2022-09-24 12:07:06 +0200

    Add cool slide effect

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,16 @@
 const menu = document.querySelector(".menu");
 const close = document.querySelector(".close");
 const nav = document.querySelector("nav");
+const features = document.querySelectorAll(".feature");
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
 
 menu.addEventListener("click", () => {
   nav.classList.add("open-nav");
@@ -9,3 +19,5 @@ menu.addEventListener("click", () => {
 close.addEventListener("click", () => {
   nav.classList.remove("open-nav");
 });
+
+features.forEach((fe) => observer.observe(fe));
